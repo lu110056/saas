a12f4b7441c5fa455447afdc005b43ae350b060c
liuht <liuhuitao890411>
2018-09-25 09:09:49 +0800

    修改cloud admin

---
+++ taroco-cloud/cloud-admin/src/main/java/cn/taroco/admin/TarocoBootAdminApplication.java
@@ -0,0 +1,34 @@
+package cn.taroco.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * taroco 服务治理service
+ *
+ * @author liuht
+ * @date 2017/11/18 20:24
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class TarocoBootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarocoBootAdminApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+}
